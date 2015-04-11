40a9333feb1bb228f3345a7d6c58e8bb57c3d04c
James Kieley <james.kieley@ableengineering.com>
2015-04-11 08:57:44 -0700

    wow, that was easy after having the model

--- src/Broker.java
+++ src/Broker.java
@@ -1,27 +1,56 @@
 import java.util.Collection;
+// @Begin
+import java.util.LinkedList;
+// @End
+
 
 public class Broker implements EventBus {
 
 	private Collection<Subscription> subscriptions;
 
 	public Broker() {
-
+		subscriptions = new LinkedList<Subscription>();
 	}
 
 	public void proccessEvent(UnsubscribeEvent event) {
-
+		// @Begin
+		Subscription subscription = getSubscriptionByProductType(event.getProductType());
+		subscription.removeConsumer(event.getConsumer());
+		// @End
 	}
 
 	public void proccessEvent(SubscribeEvent event) {
-
+		// @Begin
+		Subscription subscription = getSubscriptionByProductType(event.getProductType());
+		subscription.addConsumer(event.getConsumer());
+		// @End
 	}
 
 	public void proccessEvent(PublishEvent event) {
-
+		// @Begin
+		Subscription subscription = getSubscriptionByProductType(event.getProduct().getProductType());
+		Collection<Consumer> consumers = subscription.getConsumers();
+		for(Consumer consumer : consumers){
+			consumer.notifiy(event);
+		}
+		// @End
 	}
 
 	protected Subscription getSubscriptionByProductType(String productType) {
-		return null;
+		// @Begin
+		Subscription toReturn  = null;
+		for (Subscription subscription : subscriptions) {
+			if (subscription.getProductType().equals(productType)) {
+				toReturn = subscription;
+			}
+		}
+		if(toReturn == null){
+			toReturn = new Subscription(productType);
+			subscriptions.add(toReturn);
+		}
+		return toReturn;
+		// @End
+		// return null;
 	}
 
 }

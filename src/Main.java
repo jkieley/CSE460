6a12e3ad988cba92e3cf470f35138793e2696bda
James Kieley <james.kieley@ableengineering.com>
2015-04-11 09:18:59 -0700

    everything is implemented! start testing!

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,51 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+// @Begin
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+// @End
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("inputargs: "+String.valueOf(args.length));
-		for(String arg: args){
-			System.out.println("arg: "+arg);
-		}
+		// @Begin
+		Broker broker = new Broker();
 		File inputFile = new File(args[0]);
 		try{
 			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-		    String line;
-		    while ((line = reader.readLine()) != null) {
-		    	String[] columns = line.split(",");
-		    	for(int i=0; i<columns.length;i++){
-		    		System.out.println("column: "+columns[i]+" "+i);
-		    	}
-
-		    	String command = columns[0].trim().toLowerCase();
-		    	if (command.equals("subscribe") || command.equals("unsubscribe")) {
-		    		String consumerName = columns[1].trim().toLowerCase();
-		    		String productType = columns[2].trim().toLowerCase();
-		    		if (command.equals("subscribe")) {
-		    			Consumer consumer = ConsumerHolder.getConsumer(consumerName);
-		    		}else{ //command.equals("unsubscribe")
-
-		    		}
-		    	}else if(command.equals("publish")){
-
-		    	}else{
-		    		System.out.println("unrecgonized, first column");
-		    	}
-		    }
-		}catch(Exception e){}
+			try{
+			    String line;
+			    while ((line = reader.readLine()) != null) {
+			    	String[] columns = line.split(",");
+			    	String command = columns[0].trim().toLowerCase();
+			    	if (command.equals("subscribe") || command.equals("unsubscribe")) {
+			    		String consumerName = columns[1].trim().toLowerCase();
+			    		String productType = columns[2].trim().toLowerCase();
+			    		if (command.equals("subscribe")) {
+			    			Consumer consumer = ConsumerHolder.getConsumer(consumerName);
+			    			consumer.subscribe(broker, productType);
+			    		}else{ //command.equals("unsubscribe")
+			    			Consumer consumer = ConsumerHolder.getConsumer(consumerName);
+			    			consumer.unsubscribe(broker, productType);
+			    		}
+			    	}else if(command.equals("publish")){
+			    		String venderName = columns[1].trim().toLowerCase();
+			    		String productType = columns[2].trim().toLowerCase();
+			    		String productName = columns[3].trim().toLowerCase();
+			    		Vender vender = VenderHolder.getVender(venderName);
+			    		vender.publish(broker, new Product(productName,productType));
+			    	}else{
+			    		System.out.println("unrecgonized, first column");
+			    	}
+			    }
+			}catch(Exception e){
+				System.out.println(e.toString());
+			}finally{
+				reader.close();
+			}
+		}catch(Exception e){
+			System.out.println(e.toString());
+		}
+		// @End
 	}
 
 }
